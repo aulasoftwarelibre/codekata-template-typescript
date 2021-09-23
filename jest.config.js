abd9391ac4334a07023d5ead170f27235aa64c3f
Sergio Gómez <decano@gmail.com>
2021-09-23 17:14:35 +0200

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.spec.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
