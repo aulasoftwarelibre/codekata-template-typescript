359d96d94f9da3c3fb0d300bd629016198a123e8
Sergio Gómez <decano@gmail.com>
2025-11-15 12:23:30 +0100

    chore: align tooling configs

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,6 +1,6 @@
 import { expect, it } from 'vitest'
 
-import { isTrue } from '@/kata'
+import { isTrue } from './kata.ts'
 
 it('It is true', () => {
   expect(isTrue()).toBe(true)
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,3 @@
-import path from 'node:path'
-
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
-    },
-  },
-})
+export default defineConfig({})
